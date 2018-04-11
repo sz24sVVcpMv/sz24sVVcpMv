40f055081eaad0b7301ee5ffba6accbf7665fcfd
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2018-04-11 17:42:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,66 +4,51 @@ var bot = new Discord.Client();
 
 client.on('ready', () => {
   client.user.setStatus("online");
-  client.user.setGame("%help | ZombieBot");
+  client.user.setGame("-help");
   console.log("Reload Completed!");
 });
 
-const prefix = "%";
+const prefix = "-";
 client.on("message", (message) => {
 
   if (!message.content.startsWith(prefix)) return;
 
-  if (message.content === (prefix + "discord")) {
+  if (message.content === (prefix + "embed")) {
     message.channel.send({embed: {
       color: 0x008000,
-      title: "__Zombie Discord:__\n \n",
-      description: "** **\nhttps://discord.gg/ZqsBYV5",
+      title: "Title",
+      description: "** **\nDescription",
       footer: 
       {
-          text: "ZombieBot"
+          icon_url: client.user.avatarURL,
+          text: "TestBot"
       }
     }});
   } else
   if (message.content === (prefix + "creator")) {
     message.channel.send("The Creator of the Bot is `Derpy#6522`");
   } else
-  if (message.content === (prefix + "youtube")) {
-    message.channel.send("**Youtube Channel:**\n** **\nhttps://www.youtube.com/channel/UCKdMasMTxyCezkDItjstSHA");
-  } else
   if (message.content === (prefix + "invite")) {
     message.author.sendMessage("**__Invite The Bot:__**\n \nhttps://discordapp.com/api/oauth2/authorize?client_id=432950161778409472&permissions=0&scope=bot");
-    message.reply("Please check your direct messages :mailbox_with_no_mail:");
-  } else
-  if (message.content === (prefix + "help")) {
-    message.author.sendMessage({embed: {
-     color: 0x008000,
-     title: "__Zombie Commands:__\n \n",
-     description: "** **\n**%discord ** - Discord Link\n**%creator** - The Creators of the Bot\n**%ping** - Show to you how much Ping you have\n**%avatar** - show your avatar profile\n**%youtube** - Youtube Channel\n**%help** - Show this Menu",
-     footer: 
-     {
-         icon_url: client.user.avatarURL,
-         text: "ZombieBot"
-     }
-   }});
-    message.reply("Please check your direct messages :mailbox_with_no_mail:");
+    message.reply("Please check your direct messages :mailbox_with_no_mail:")
   }
 });
 
   client.on('message', message => {
-    if (message.content === `%ping`) {
+    if (message.content === `-ping`) {
       message.reply(`Pong! The ping is **${(client.ping).toFixed(0)}**ms!  :ping_pong:`);
     }
   });
 
   client.on('message', message => {
-    if (message.content === '%avatar') {
+    if (message.content === '-avatar') {
       message.reply(message.author.avatarURL);
     }
   });
 
   client.on('message', message => {
     if (message.content === 'test') {
-      message.reply("I am Online, for start type `%help`");
+      message.reply("I am Online");
     }
   });
 
