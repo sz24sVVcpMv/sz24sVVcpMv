ee097ff40b3a49deebc56f960ee61446e531cdf2
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2018-04-23 20:12:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = process.env.BOT_PREFIX;
+  let prefix = ">";
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
