22b0e8cae56d04f1cff90ab8d32a60d633d45038
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2018-04-24 16:35:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const botconfig = require("./botconfig.json");
+
+const bot = new Discord.Client({disableEveryone: true});
 
 client.on('ready', () => {
 client.user.setGame(`Test Bots | >help`);
   console.log(`Bot is Online!`);
 });
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
 
   let prefix = botconfig.prefix;
 
