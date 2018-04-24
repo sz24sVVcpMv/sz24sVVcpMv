b34a6814f91cb90adf57b1f4a2724381431cb1cb
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2018-04-24 16:22:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-var servers = {};
-
-const bot = new Discord.Client({disableEveryone: true});
-
-bot.on('ready', () => {
-bot.user.setGame(`Test Bots | >help`);
+client.on('ready', () => {
+client.user.setGame(`Test Bots | >help`);
   console.log(`Bot is Online!`);
 });
 
   let prefix = ">";
 
-bot.on('message', msg => {
+client.on('message', msg => {
   if (msg.content === '${prefix}ping') {
     msg.reply('Pong!');
   }
