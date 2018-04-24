9289b82f5cf6e7dba282dc2f6e589ac89ec3bfbd
Derpy666 <37512753+Derpy666@users.noreply.github.com>
2018-04-24 16:09:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var servers = {};
+
+const bot = new Discord.Client({disableEveryone: true});
+
 bot.on('ready', () => {
 bot.user.setGame(`Test Bots | >help`);
   console.log(`Bot is Online!`);
