12877f412add6ed71da7479317270fd22a41663e
9oS01frMn4k <38668261+9oS01frMn4k@users.noreply.github.com>
2018-10-03 15:31:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const client = new Discord.Client();
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  client.user.setActivity(`${client.users.size} שחקנים | By NiceGames & Date`);
+  client.user.setGame(`SupportBot | By NiceGames & Date`);
 });
 
 client.on("message", async message => {
