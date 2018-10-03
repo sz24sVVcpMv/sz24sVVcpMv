c88bdbd99daaaebd3efda69b1bc5c50fc4719318
9oS01frMn4k <38668261+9oS01frMn4k@users.noreply.github.com>
2018-10-03 15:25:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const client = new Discord.Client();
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  client.user.setActivity(`${client.users.size} שחקנים | By NiceGames & Date`);
-    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-  .catch (console.error);
+  client.user.setGame(`${client.users.size} שחקנים | By NiceGames & Date`);
 });
 
 client.on("message", async message => {
